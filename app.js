a6ad9832e9480a218aa87bd9d54b6e25d0b13a37
Giacomo Cerreto <frookes@pm.me>
2019-02-10 22:20:10 +0100

    thanks giovanni

---
+++ app.js
@@ -0,0 +1,32 @@
+const { _: [sqlfile] } = require("simple-argv")
+if (!sqlfile) {
+  throw new Error("devi specificare il nome del file sql da eseguire come primo argomento del comando")
+}
+console.log(`eseguo ${sqlfile}.sql`)
+const mysql = require("mysql")
+const config = require("./dbconfig.json")
+const { readFileSync } = require("fs")
+const { join } = require("path")
+
+const sql = readFileSync(join(__dirname, `${sqlfile}.sql`), "utf8")
+
+const pool = mysql.createPool({
+  connectionLimit: 10,
+  multipleStatements: true,
+  ...config
+})
+
+pool.query(sql, (err, data) => {
+  if (err) {
+    console.log(err)
+  } else {
+    console.log(data)
+    pool.end((err) => {
+      if (err) {
+        console.log(err)
+      } else {
+        console.log("closed")
+      }
+    })
+  }
+})
