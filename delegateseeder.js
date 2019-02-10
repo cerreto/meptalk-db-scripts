04b8c4e01ac0d90064901cc9c008da97cafc67dd
Giacomo Cerreto <frookes@pm.me>
2019-02-10 22:36:59 +0100

    quite finished

---
+++ delegateseeder.js
@@ -0,0 +1,1 @@
+// todo: script js che prenda i dati da un csv
---
+++ schoolseder.sql
@@ -0,0 +1,14 @@
+INSERT INTO schools(id, name, speeches) VALUES
+(1, "Ariosto-Spallanzani", 0),
+(2, "Bassi-Burgatti", 0),
+(3, "Carducci", 0),
+(4, "Cevolani", 0),
+(5, "Cicognini", 0),
+(6, "Corni", 0),
+(7, "Fanti", 0),
+(8, "Luosi", 0),
+(9, "Muratori-San Carlo", 0),
+(10, "Meucci", 0),
+(11, "Roiti", 0),
+(12, "Selmi", 0),
+(13, "Tassoni", 0);
